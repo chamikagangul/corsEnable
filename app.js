f1b3a5390c3ec9338b4da8c53894dcb41fbeb673
Chamika_112085 <Chamika.Mahiepala@axiatadigitallabs.com>
2020-12-09 18:44:43 +0530

    .

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const fetch = require('node-fetch');
+const cors = require("cors");
 
 
 
@@ -13,6 +14,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(cors);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
